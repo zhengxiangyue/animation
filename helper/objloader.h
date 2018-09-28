69355f5628126955fad0b201ca2ea72bebaf33c5
zhengxiangyue <zhengxiangyue@zhengxiangyues-MacBook-Pro.local>
2018-09-28 09:53:30 -0400

    Fri Sep 28

--- helper/objloader.h
+++ helper/objloader.h
@@ -18,14 +18,4 @@ bool loadOBJ(
 	float scale
 );
 
-
-
-bool loadAssImp(
-	const char * path, 
-	std::vector<unsigned short> & indices,
-	std::vector<glm::vec3> & vertices,
-	std::vector<glm::vec2> & uvs,
-	std::vector<glm::vec3> & normals
-);
-
 #endif
